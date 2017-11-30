82bb250fc95d04b5407581d7a22fa53d620af7da
Jennifer <jennifer.gd42@gmail.com>
2017-11-30 11:15:50 -0800

    First commit.

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, session, jsonify, request
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+import os
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "DEVKEY")
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def show_index():
+    """The homepage."""
+
+    return "Let's track our runs!"
+
+
+if __name__ == "__main__":
+    app.debug = os.environ.get("DEBUG") or False
+
+    if app.debug:
+        DebugToolbarExtension(app)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # connect_to_db(app)
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
