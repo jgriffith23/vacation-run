e5fe6ddb448e7bec6de858cda40d249e96009c68
Jennifer <jennifer.gd42@gmail.com>
2017-12-11 23:34:47 -0800

    Remove smoke test

--- tests.py
+++ tests.py
@@ -2,18 +2,6 @@ import unittest
 from server import app
 
 
-class SmokeTest(unittest.TestCase):
-    """Confirm that the tests can run."""
-
-    def setUp(self):
-        app.config['TESTING'] = True
-        app.secret_key = "ABC"
-        self.client = app.test_client()
-
-    def test_smoke(self):
-        self.assertEqual("Does this pass?", "It shouldn't!")
-
-
 class BasicTests(unittest.TestCase):
     """Test features that don't require login or database."""
 
